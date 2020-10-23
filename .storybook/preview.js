6edc84a4125d166f916aeb78882cefd04e510f26
xuwenjie <fivelucky@126.com>
2020-10-23 17:25:37 +0800

    feat：更新

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: { expanded: true },
+  // backgrounds: {
+  //   values: [
+  //     { name: 'red', value: '#f00' },
+  //     { name: 'green', value: '#0f0' },
+  //   ],
+  // },
+  // backgroundColor: {
+  //   value: 'red',
+  //   control: {
+  //     type: 'color'
+  //   }
+  // },
 }
